ed1e7a2d75b4042afee34ebb38b89ca19c2ecae6
easydaniel <whitetiger50512@gmail.com>
2017-01-12 19:49:39 +0800

    init

---
+++ Debug/Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/system_stm32l4xx.c 
+
+OBJS += \
+./Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/system_stm32l4xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/system_stm32l4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/%.o: ../Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32L476xx -I"/Users/Daniel/Documents/workspace/ttttt/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/STM32L4xx_HAL_Driver/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy" -I"/Users/Daniel/Documents/workspace/ttttt/Middlewares/ST/STM32_USB_Host_Library/Core/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Middlewares/ST/STM32_USB_Host_Library/Class/HID/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/CMSIS/Device/ST/STM32L4xx/Include" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid.c \
+../Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_keybd.c \
+../Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_mouse.c \
+../Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_parser.c 
+
+OBJS += \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid.o \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_keybd.o \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_mouse.o \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_parser.o 
+
+C_DEPS += \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid.d \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_keybd.d \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_mouse.d \
+./Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/usbh_hid_parser.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/%.o: ../Middlewares/ST/STM32_USB_Host_Library/Class/HID/Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -D__weak="__attribute__((weak))" -D__packed="__attribute__((__packed__))" -DUSE_HAL_DRIVER -DSTM32L476xx -I"/Users/Daniel/Documents/workspace/ttttt/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/STM32L4xx_HAL_Driver/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy" -I"/Users/Daniel/Documents/workspace/ttttt/Middlewares/ST/STM32_USB_Host_Library/Core/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Middlewares/ST/STM32_USB_Host_Library/Class/HID/Inc" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/CMSIS/Device/ST/STM32L4xx/Include" -I"/Users/Daniel/Documents/workspace/ttttt/Drivers/CMSIS/Include"  -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
